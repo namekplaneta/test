b1927c6123c6b54ba7fa21b4e6a69a145f57c41d
namekplaneta <83769912+namekplaneta@users.noreply.github.com>
2021-05-06 15:44:07 +0200

    Bihotz taupadak

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
-input.onSound(DetectedSound.Loud, function () {
-	
-})
+basic.showNumber(3)
+basic.showNumber(2)
+basic.showNumber(1)
 basic.forever(function () {
-	
+    basic.showIcon(IconNames.Heart)
+    basic.showIcon(IconNames.SmallHeart)
 })
